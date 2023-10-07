6cce20fa66ce7d8851f42c44214b6e700d086c48
DasIstMars <hledger@bk.ru>
2023-10-07 19:57:48 +0300

    1220px

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: "auto",
-    spaceBetween: 16,
-    speed: 500,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: true,
-    },
-  });
+let mySwiper;
+const breakpoint = window.matchMedia( '(min-width:768px)' );
+const breakpointChecker = function() {
+  if ( breakpoint.matches === true ) {
+     if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
+     return;
+  } else if ( breakpoint.matches === false ) {
+     return enableSwiper();
+  }
+};
+const enableSwiper = function() {
+  mySwiper = new Swiper('.swiper', {
+      loop: true,
+      slidesPerView: "auto",
+      spaceBetween: 16,
+      speed: 500,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+          delay: 2000,
+          disableOnInteraction: true,
+      },
+    });
+};
+breakpoint.addListener(breakpointChecker);
+breakpointChecker();
+
+let expantButton = document.querySelector('.s-expand-button')
+let expand = document.querySelector('.s-nav--minimalize')
+
+expantButton.onclick = function() {
+  expand.classList.toggle('s-nav--expanded');
+};
